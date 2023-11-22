4dca8e71778ed0fde3f092c03810e83e6631c3f3
paulina koch <pjassakoch@gmail.com>
2023-11-21 20:12:28 -0500

    Added start timer when the startbutton is clicked

--- Assets/script.js
+++ Assets/script.js
@@ -11,65 +11,152 @@
 
 */
 
-let question =  document.querySelector(".title");
-let questionContainerEl =document.querySelector(".button");
+let questionEl = document.querySelector(".title");
+let questionContainerEl = document.querySelector(".button");
 let multipleChoiceOption1 = document.querySelector("#choice1");
 let multipleChoiceOption2 = document.querySelector("#choice2");
 let multipleChoiceOption3 = document.querySelector("#choice3");
 let multipleChoiceOption4 = document.querySelector("#choice4");
+let anyMultipleChoiceButton = document.querySelector(".button");
 let startButton = document.querySelector(".start-button");
+let trackingNumber = 0;
+let timerElement = document.querySelector("#timer");
+let score = 0;
 
+let timeLeft = 60;
 
 //array of question objects:
-//let questionArray = [
-   ""
-//
+let questionArray = [
+    {
+        question: "Commonly used data types DO NOT Include:",
+
+        answer: 'alerts',
+        choiceA: 'strinrg',
+        choiceB: 'booleans',
+        choiceC: 'numbers',
+
+    },
+    {
+        question: "The condition in an if/else statment is enclosed with _______",
+
+        answer: 'parenthesis',
+        choiceA: 'quotes',
+        choiceB: 'curly brackets',
+        choiceC: 'square brackets',
+
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store______:",
+
+        answer: 'all true',
+        choiceA: 'numbers and strings',
+        choiceB: 'other arrays',
+        choiceC: 'booleans',
+
+    },
+
+    {
+        question: "String values must be enclosed within_____when being assigned to variables.",
+
+        answer: 'quotes',
+        choiceA: 'commas',
+        choiceB: 'curly brackets',
+        choiceC: 'parenthesis',
+
+    },
+
+]
+
 
 function startGame() {
+    questionEl.textContent = questionArray[trackingNumber].question;
+    multipleChoiceOption1.textContent = questionArray[trackingNumber].choiceA;
+    multipleChoiceOption2.textContent = questionArray[trackingNumber].choiceB;
+    multipleChoiceOption3.textContent = questionArray[trackingNumber].choiceC;
+    multipleChoiceOption4.textContent = questionArray[trackingNumber].answer;
+
     console.log('Started');
-startButton.classList.add('hide');
-questionContainerEl.classList.remove('hide');
 
-
-//Prevents start button from being clicked when round is in progress
-startButton.disabled = true;
+    //Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
 
 }
 
-
 //Add event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame)
+startButton.addEventListener("click", startGame);
 
+startButton.addEventListener("click", startTimer);
+multipleChoiceOption1.addEventListener("click", selectAnswer);
+multipleChoiceOption2.addEventListener("click", selectAnswer);
+multipleChoiceOption3.addEventListener("click", selectAnswer);
+multipleChoiceOption4.addEventListener("click", selectAnswer);
 //create function to start the quiz when the start button is clicked
 
 
 //this function will make the next question appear once the user clicks
+//need for loop for next question?
 function nextQuestion() {
+    //have to have if statement to check if statement that they went index not greater than 3 then game over, if lower then display next question
+    if (questionArray[trackingNumber] < 3) {
+        anyMultipleChoiceButton.addEventListener("click")
+        console.log("game over")
+        return
 
+    } else if  (){
 
+        questionEl.textContent = questionArray[trackingNumber].question;
+        multipleChoiceOption1.textContent = questionArray[trackingNumber].choiceA;
+        multipleChoiceOption2.textContent = questionArray[trackingNumber].choiceB;
+        multipleChoiceOption3.textContent = questionArray[trackingNumber].choiceC;
+        multipleChoiceOption4.textContent = questionArray[trackingNumber].answer;
+        console.log('Started');
+
+    }
 }
 
-//When user selects answer a messaage will appear correct or wrong
-function selectAnswer() {
+    //When user selects answer a messaage will appear correct or wrong
+    //increase score and increase tracking number and display new set of questions
+    function selectAnswer(event) {
+        if (event.target.textContent == questionArray[trackingNumber].answer) {
+            console.log("correct answer")
+            //else decrease score and increase tracking number and display next set of questions
+        } else { }
+    
+    }
 
-}
-
-//timer will run while the user is answering questons
-function starTimer () {
 
 
-}
+    //timer will run while the user is answering questons
+    function startTimer() {
+        let timer = setInterval(function () {
+            timeLeft--;
 
+            timerElement.textContent = timeLeft;
+            if (timeLeft === 0) {
+                // reset timer
+                clearInterval(timer);
+                return
+
+            }
+
+        }
+            , 1000);
+
+    }
+
+
+/*
 
 
 //This function will execute when the correct choice is selected
-function correct () {
+function correct() {
 
 }
 
 
-//this function will exexute when the wrong answer is selected
-function wrong () {
+//this funciton will exectute when the answer is wrong
+function wrong() {
 
-    
 }
+*/
